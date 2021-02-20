59c020c916d54de7b891c3d9ce2e87af8eb76981
FanniBelics <fanni.belics@gmail.com>
2021-02-20 18:45:17 +0100

    A 3. pontban említett változtatások elvégezve

--- Drillek/Drill12/main.cpp
+++ Drillek/Drill12/main.cpp
@@ -6,22 +6,24 @@ int main()
     using namespace Graph_lib;
 
     Point tl {100, 100};
-    Simple_window win {tl, 600, 400, "My Window"};
+    Simple_window win {tl, 1200, 800, "My Window"};
 
-    Axis xa{Axis::x, Point {20,300},280,10,"x axis"};
-    Axis ya{Axis::y, Point {20,300}, 280, 10, "y axis"};
+    Axis xa{Axis::x, Point {100,300},600,10,"x axis"};
+    xa.set_color(Color::blue);
+    xa.label.set_color(Color::dark_blue);
+    Axis ya{Axis::y, Point {100,300}, 600, 10, "y axis"};
     ya.set_color(Color::red);
     ya.label.set_color(Color::dark_red);
 
-    Function sine(sin,0,100,Point {20,150},1000,50,50);
+    Function sine(sin,0,100,Point {20,150},1000,100,100);
 
     Polygon poly;
-    poly.add(Point {300, 200});
-    poly.add(Point {350, 100});
-    poly.add(Point {400, 200});
-    poly.set_color(Color::red);
-    poly.set_style(Line_style::dash);
-    poly.set_style(Line_style(Line_style::dash,4));
+    poly.add(Point {600, 700});
+    poly.add(Point {650, 600});
+    poly.add(Point {700, 700});
+    poly.set_color(Color::cyan);
+    poly.set_style(Line_style::dot);
+    poly.set_style(Line_style(Line_style::dot,4));
 
     Rectangle r {Point {200, 200}, 200, 50};
     r.set_fill_color(Color::yellow);
@@ -32,21 +34,23 @@ int main()
     poly_rect.add(Point {200, 100});
     poly_rect.add(Point {100, 100});
     poly_rect.add(Point {50, 75});
-    poly_rect.set_style(Line_style(Line_style::dash,2));
+    poly_rect.set_style(Line_style(Line_style::dashdot,3));
 
     Text t {Point {150, 150}, "Hello, graphical world!"};
-    t.set_font(Font::times_bold);
-    t.set_font_size(20);
+    t.set_font(Font::courier);
+    t.set_font_size(30);
+    t.set_color(Color::dark_green);
 
-    Image ii {Point {100,50}, "image.jpg"};
+    Image ii {Point {0,40}, "image.jpg"};
     //ii.move(100,200); szegmentációs hibát okozott
 
-    Circle c {Point {100, 200},50};
+    Circle c {Point {600, 400},210};
+    c.set_fill_color(Color::green);
     /*
     Ellipse e {Point {10,,200}, 75, 25};
     e.set_color{Color::dark_red};
     */
-    Mark m {Point {100,200}, 'x'};
+    Mark m {Point {100,200}, '+'};
 
     win.attach(ii);
     win.attach(xa);
