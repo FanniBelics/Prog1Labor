cce091075e536a796d76fc013cd138b9b221d004
FanniBelics <fanni.belics@gmail.com>
2021-03-10 16:29:46 +0100

    Védés előtti hibaellenőrzés

--- Drillek/Drill12/main.cpp
+++ Drillek/Drill12/main.cpp
@@ -1,3 +1,4 @@
+
 #include "Simple_window.h"
 #include "Graph.h"
 
@@ -37,19 +38,16 @@ int main()
     poly_rect.set_style(Line_style(Line_style::dashdot,3));
 
     Text t {Point {150, 150}, "Hello, graphical world!"};
-    t.set_font(Font::courier);
+    t.set_font(Graph_lib::Font::courier);
     t.set_font_size(30);
     t.set_color(Color::dark_green);
 
     Image ii {Point {0,40}, "image.jpg"};
-    //ii.move(100,200); szegmentációs hibát okozott
 
     Circle c {Point {600, 400},210};
     c.set_fill_color(Color::green);
-    /*
-    Ellipse e {Point {10,,200}, 75, 25};
-    e.set_color{Color::dark_red};
-    */
+    Ellipse e {Point {100,200}, 75, 25};
+    e.set_color(Color::dark_red);
     Mark m {Point {100,200}, '+'};
 
     win.attach(ii);
@@ -60,8 +58,12 @@ int main()
     win.attach(r);
     win.attach(poly_rect);
     win.attach(t);
+    win.attach(e);
     win.attach(c);
     win.attach(m);
+
+    win.wait_for_button();
+    ii.move(10,20);
 
     win.wait_for_button();
 }
