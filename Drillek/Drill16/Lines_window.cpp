61a1bd0292bd14588ec59844a40c143aba615399
FanniBelics <fanni.belics@gmail.com>
2021-04-04 13:56:37 +0200

    Többszín hozzáadva, pop-up menü hozzáadva

--- Drillek/Drill16/Lines_window.cpp
+++ Drillek/Drill16/Lines_window.cpp
@@ -9,7 +9,8 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     next_x{Point{300,0}, 50, 40, "next x:"},
     next_y{Point{450,0}, 50, 40, "next y:"},
     xy_out{Point{100,0}, 100, 40, "current (x,y):"},
-    color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"color"}
+    color_menu{Point{x_max()-70,40},70,20,Menu::vertical,"color"},
+    color_button{Point{x_max()-120,40}, 120, 45, "color menu", cb_color_menu}
 {
     attach(next_button);
     attach(quit_button);
@@ -19,11 +20,16 @@ Lines_window::Lines_window(Point xy, int w, int h, const string& title)
     attach(lines);
 
     color_menu.attach(new Button{Point{0,0},0,0,"red",cb_red});
+    color_menu.attach(new Button{Point{0,0},0,0,"scarlet",cb_dark_red});
     color_menu.attach(new Button{Point{0,0},0,0,"blue",cb_blue});
-    color_menu.attach(new Button{Point{0,0},0,0,"black",cb_black});
+    color_menu.attach(new Button{Point{0,0},0,0,"ink",cb_dark_blue});
     color_menu.attach(new Button{Point{0,0},0,0,"white",cb_white});
+    color_menu.attach(new Button{Point{0,0},0,0,"black",cb_black});
 
     attach(color_menu);
+    color_menu.hide();
+    attach(color_button);
+
 
 }
 
--- Drillek/Drill16/Lines_window.h
+++ Drillek/Drill16/Lines_window.h
@@ -8,13 +8,18 @@ struct Lines_window : Graph_lib::Window {
 
 	Menu color_menu;
 
+	Button color_button;
+
 	bool wait_for_button();
 
 	//CallBacks
 	static void cb_red(Address, Address pw) {reference_to<Lines_window>(pw).red_pressed();}
+	static void cb_dark_red(Address, Address pw){reference_to<Lines_window>(pw).dark_red_pressed();}
 	static void cb_blue(Address, Address pw) {reference_to<Lines_window>(pw).blue_pressed();}
+	static void cb_dark_blue(Address, Address pw) {reference_to<Lines_window>(pw).dark_blue_pressed();}
 	static void cb_black(Address, Address pw){reference_to<Lines_window>(pw).black_pressed();}
 	static void cb_white(Address, Address pw){reference_to<Lines_window>(pw).white_pressed();}
+	static void cb_color_menu(Address, Address pw){reference_to<Lines_window>(pw).color_pressed();}
 
 private:
 	Button next_button;
@@ -31,9 +36,15 @@ private:
 	//setting colors
 	void change(Color c) {lines.set_color(c);}
 
-	void red_pressed() {change(Color::red);}
-	void blue_pressed() {change(Color::blue);}
-	void black_pressed(){change(Color::black);}
-	void white_pressed(){change(Color::white);}
+	void red_pressed() {change(Color::red);hide_menu();}
+	void dark_red_pressed(){change(Color::dark_red);hide_menu();}
+	void blue_pressed(){change(Color::blue);hide_menu();}
+	void dark_blue_pressed(){change(Color::dark_blue);hide_menu();}
+	void white_pressed(){change(Color::white);hide_menu();}
+	void black_pressed(){change(Color::black);hide_menu();}
+
+	//part3
+	void hide_menu(){color_menu.hide();color_button.show();}
+	void color_pressed(){color_button.hide();color_menu.show();}
 
 };
